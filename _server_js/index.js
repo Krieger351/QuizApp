477db34f7f697156ead73615f10006b273921b53
VladimirStrelok <alexander.glica@gmail.com>
2015-11-24 10:36:55 -0500

    change port, add some of the questions

--- _server_js/index.js
+++ _server_js/index.js
@@ -64,6 +64,6 @@ io.sockets.on('connection', function (socket) {
     })
 
 });
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
